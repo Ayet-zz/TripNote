6d8fdc200e2c1fe382f88f24df712bbc9389fa7f
LothaireLaeuffer <lothaire.laeuffer@gmail.com>
2017-11-15 15:59:11 +0100

    merge

--- app/src/main/java/com/tbuonomo/jawgmapsample/StoriesFragment.java
+++ app/src/main/java/com/tbuonomo/jawgmapsample/StoriesFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,7 +71,8 @@ public class StoriesFragment extends Fragment{
 
 
 
-        new ReadFileTask( myDataSet, new ReadFileTask.OnTestListener() {
+        new ReadFileTask( myDataSet,(ProgressBar)getActivity().findViewById(R.id.progressBar), new ReadFileTask.OnTestListener() {
+
             @Override
             public void OnSuccess() {
 
